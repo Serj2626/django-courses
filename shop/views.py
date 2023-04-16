859a8728e09ebba0a83b2752b51582bf0b1d6307
Sergey Boytsov <serj2626@mail.ru>
2023-04-16 16:15:36 +0300

    new information in shop.views

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Course
 
 
 def index(request):
-    return HttpResponse('Hello from the Shop app')
+    courses = Course.objects.all()
+    res = [str(course) + '<br>' for course in courses]
+    return HttpResponse(res)
